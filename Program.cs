716336f6da32b16e3bcd841e02a340b9b222c5dc
CasperKirud <ckirud@gmail.com>
2024-04-30 15:17:56 +0200

    small change

--- Program.cs
+++ Program.cs
@@ -2,12 +2,10 @@ using microservice_characterManagement;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddHealthChecks(); // Register health checks
+builder.Services.AddHealthChecks();
 
-// Add CORS services
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policyBuilder =>
@@ -18,22 +16,19 @@ builder.Services.AddCors(options =>
     });
 });
 
-// Register controllers
 builder.Services.AddControllers();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.MapHealthChecks("/health"); // Map the health checks endpoint
+app.MapHealthChecks("/health");
 
 app.UseHttpsRedirection();
-app.UseCors("AllowAll"); // Apply CORS policy
+app.UseCors("AllowAll");
 
-// Add routes and controllers if you have any
-app.MapControllers(); // Ensure you have controllers added to your project
+app.MapControllers();
 
 app.Run();
 
